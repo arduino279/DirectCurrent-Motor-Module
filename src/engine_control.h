541a64cb86110f8d03441599465024152af441da
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-11-30 08:45:53 +0100

    Update engine_control.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/engine_control.h
+++ src/engine_control.h
@@ -1,9 +1,2 @@
 #include <servo_component.h>
 #include <dc_component.h>
-
-void dc_control(int pin, int dc_pin, int mode, int speed) {
-  digitalWrite(pin, mode);
-  if(speed != "") {
-    analogWrite(engine_id, speed);
-  }
-}
