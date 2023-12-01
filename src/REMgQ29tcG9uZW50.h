6ece9f4495811002dde781c94ed2ffb65c8cb333
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-12-01 13:26:10 +0100

    Update REMgQ29tcG9uZW50.h

--- src/REMgQ29tcG9uZW50.h
+++ src/REMgQ29tcG9uZW50.h
@@ -1,4 +1,15 @@
-void dc(int pin, int dc_pin, int mode, int speed) {
+int attachedInput1 = 0;
+int attachedInput2 = 0;
+
+void attachInput1(int pin) {
+  attachedInput1 = pin;
+}
+
+void attachInput2(int pin) {
+  attachedInput2 = pin;
+}
+
+void moveDC(int pin, int mode, int speed) {
   digitalWrite(pin, mode);
   if(speed != "") {
     analogWrite(engine_id, speed);
