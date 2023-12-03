c74d2a5fcce09a3e0b783011cf8309d69634d045
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-12-03 09:56:30 +0100

    Update REMgQ29tcG9uZW50.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/REMgQ29tcG9uZW50.h
+++ src/REMgQ29tcG9uZW50.h
@@ -2,14 +2,6 @@ int attachedInput1 = 0;
 int attachedInput2 = 0;
 int attachedEngine = 0;
 
-void attachInput1(int pin) {
-  attachedInput1 = pin;
-}
-
-void attachInput2(int pin) {
-  attachedInput2 = pin;
-}
-
 void attachEngine(int pin) {
   attachedEngine = pin;
 }
