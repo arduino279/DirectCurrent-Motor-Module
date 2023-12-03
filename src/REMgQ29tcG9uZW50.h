6afb70df7d0589a5de2987ad1ebb37f8773b5ada
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-12-03 10:13:42 +0100

    Update REMgQ29tcG9uZW50.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/REMgQ29tcG9uZW50.h
+++ src/REMgQ29tcG9uZW50.h
@@ -3,33 +3,10 @@ struct setup {
     int input2;
     int dc;
 };
- 
-my_point2.x = 4;
 
-int attachedInput1 = 0;
-int attachedInput2 = 0;
-int attachedEngine = 0;
-
-void attachEngine(int pin) {
-  attachedEngine = pin;
-}
-
-void attach(int input1, int input2, int engine) {
-  attachInput1(input1);
-  attachInput2(input2);
-  attachEngine(engine);
-}
-
-void Input1(int mode, const speed) {
-  digitalWrite(attachedInput1, mode);
+void moveDC(int mode, const speed) {
+  digitalWrite(setup.input1, mode);
   if(speed != "") {
-    analogWrite(attachedEngine, speed);
-  }
-}
-
-void Input2(int mode, int speed) {
-  digitalWrite(attachedInput2, mode);
-  if(speed != "") {
-    analogWrite(attachedEngine, speed);
+    analogWrite(setup.dc, speed);
   }
 }
