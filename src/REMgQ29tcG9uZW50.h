fa037ea61d26a97747817bcb707cdc69e645297f
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-12-04 13:48:30 +0100

    Update REMgQ29tcG9uZW50.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/REMgQ29tcG9uZW50.h
+++ src/REMgQ29tcG9uZW50.h
@@ -4,10 +4,8 @@ struct setup {
     int dc;
 };
 
-void moveDirectCurrent(int i, int mode1, int mode2, const speed) {
+void moveDirectCurrent(int i, int mode1, int mode2, int speed) {
   digitalWrite(pin[i].input1, mode2);
   digitalWrite(pin[i].input2, mode2);
-  if(speed != "") {
-    analogWrite(pin[i].dc, speed);
-  } 
+  analogWrite(pin[i].dc, speed);
 }
