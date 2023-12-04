9b0eb3b895865dfa974711a2e557506a955962a3
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-12-04 19:49:59 +0100

    Update U2Vydm8gQ29tcG9uZW50.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/U2Vydm8gQ29tcG9uZW50.h
+++ src/U2Vydm8gQ29tcG9uZW50.h
@@ -4,12 +4,14 @@
 
 typedef struct setup2 {
   int servo;
+  int angle;
 } ServoConfig;
 
 Servo servo;
 ServoConfig pin1;
 
 void moveServo(int angle) {
+  pin1.angle = angle;
   servo.attach(pin1.servo);
   servo.write(angle);
 }
