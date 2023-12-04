1ec65f568791415ac34131ba35f858c073b21e89
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-12-04 15:11:52 +0100

    Update REMgQ29tcG9uZW50.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/REMgQ29tcG9uZW50.h
+++ src/REMgQ29tcG9uZW50.h
@@ -1,8 +1,11 @@
-struct setup1 {
+#include <stdio.h>
+#include <string.h>
+
+typedef struct setup1 {
     int input1;
     int input2;
     int dc;
-};
+} DirectCurrentConfig;
 
 void moveDirectCurrent(int i, int mode1, int mode2, int speed) {
   digitalWrite(pin[i].input1, mode2);
