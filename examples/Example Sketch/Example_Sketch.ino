849102d968d518b6613d134ccf7afa43c091aff8
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-12-06 14:52:32 +0100

    Update Example_Sketch.ino
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- examples/Example Sketch/Example_Sketch.ino
+++ examples/Example Sketch/Example_Sketch.ino
@@ -1,4 +1,9 @@
 #include <DirectCurrent-Motor-Module.h>
 
-DirectCurrent example = { 1, 2, 3 }; /* Setup an class object with INPUT1 pin = 1, INPUT2 pin = 2 and DC motor pin = 3 */
-example.move(example, 1, 0, 129); /* Move the DC motor with a speed of 129 and by using the INPUT1 pin to set the motor rotating forward */
+void setup() {
+  DirectCurrent example = { 1, 2, 3 }; /* Setup an class object with INPUT1 pin = 1, INPUT2 pin = 2 and DC motor pin = 3 */
+}
+
+void loop() {
+  example.move(example, 1, 0, 129); /* Move the DC motor with a speed of 129 by using the INPUT1 pin to set the motor rotating forward */
+}
