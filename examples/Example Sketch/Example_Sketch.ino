2b5ce74a8ed6e0ff36ac0be74a15b24d5b245b9f
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-12-06 14:34:07 +0100

    Update Example_Sketch.ino
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- examples/Example Sketch/Example_Sketch.ino
+++ examples/Example Sketch/Example_Sketch.ino
@@ -1,4 +1,4 @@
 #include <DirectCurrent-Motor-Module.h>
 
-DirectCurrent example = { 1, 2, 3 }; /* Move it */
+DirectCurrent example = { 1, 2, 3 }; /* Setup an class object with INPUT1 pin = 1, INPUT2 = 2 and the pin of the DC = 3 */
 example.move(example);
