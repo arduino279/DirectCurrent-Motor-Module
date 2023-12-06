5ec1553ccfcdb8a83975e4e1aa4201c54d020c44
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-12-06 14:32:27 +0100

    Update and rename examples/dc_controller.ino to examples/Example Sketch/Example_Sketch.ino
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

---
+++ examples/Example Sketch/Example_Sketch.ino
@@ -0,0 +1,4 @@
+#include <DirectCurrent-Motor-Module.h>
+
+DirectCurrent example = { 1, 2, 3 };
+example.move(example);
--- examples/dc_controller.ino
+++
@@ -1,4 +0,0 @@
-#include <Engine_Control.h>
-
-struct setup pin1 = {2, 5, 3};
-moveDirectCurrent(1, 0, 1, 140);
