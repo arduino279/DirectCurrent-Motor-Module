1287ddf6f3d04ec46efdaf11f936290f4e60b6b6
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-12-06 14:14:14 +0100

    Update DirectCurrent-Motor-Module.h
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- src/DirectCurrent-Motor-Module.h
+++ src/DirectCurrent-Motor-Module.h
@@ -3,15 +3,11 @@ class DirectCurrent {
       int input1;
       int input2;
       int dc;
-      void move(int mode1, int mode2, int speed) {
-          digitalWrite(pin[i].input1, mode2);
-          digitalWrite(pin[i].input2, mode2);
-          analogWrite(pin[i].dc, speed);
-      }
+      move(DirectCurrent i, int mode1, int mode2, int speed);
 };
 
-void moveDirectCurrent(int i, int mode1, int mode2, int speed) {
-  digitalWrite(pin[i].input1, mode2);
-  digitalWrite(pin[i].input2, mode2);
-  analogWrite(pin[i].dc, speed);
+void DirectCurrent::move(DirectCurrent i, int mode1, int mode2, int speed) {
+    digitalWrite(i.input1, mode2);
+    digitalWrite(i.input2, mode2);
+    analogWrite(i.dc, speed);
 }
